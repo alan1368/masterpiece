2ec9cdecfeee3f915e72c1ac13f460cf69a61ad0
Alan Ashrafi <alan_1368@yahoo.com>
2022-04-23 20:12:46 +0200

    bug

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import type { NextPage } from 'next'
+//import type { NextPage } from 'next'
 import Head from 'next/head'
 import Hero from '../components/Hero'
 import sanityClient from '../lib/client'
@@ -6,11 +6,11 @@ import Post from '../components/Post'
 import imageUrlBuilder from '@sanity/image-url'
 
 const builder = imageUrlBuilder(sanityClient)
-export function urlFor(source: any) {
+export function urlFor(source) {
   return builder.image(source)
 }
 
-const Home: NextPage = ({ posts }: any) => {
+const Home = ({ posts }) => {
   return (
     <>
       <Head>
@@ -19,7 +19,7 @@ const Home: NextPage = ({ posts }: any) => {
       </Head>
       <Hero />
       <div className="mx-auto mt-5 flex w-11/12 max-w-7xl flex-col items-start">
-        {posts.map((post: any) => {
+        {posts.map((post) => {
           return (
             <Post
               title={post.title}
