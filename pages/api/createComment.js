ccb607e0b27e87d51475104202621f41f79ad787
Alan Ashrafi <alan_1368@yahoo.com>
2022-04-23 20:32:55 +0200

    bug

--- pages/api/createComment.js
+++ pages/api/createComment.js
@@ -1,7 +1,7 @@
-import client from '../../lib/client'
+import sanityClient from '../../client'
 export default async function createComment(req, res) {
   const { name, email, text, _id } = JSON.parse(req.body)
-  client?.create({
+  sanityClient?.create({
     _type: 'comment',
     name,
     text,
