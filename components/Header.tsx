0d9b0ebd44700bb18c2a26f61f440fc39c54a55d
Alan Ashrafi <alan_1368@yahoo.com>
2022-07-18 23:15:20 +0200

    style changed

--- components/Header.tsx
+++ components/Header.tsx
@@ -21,7 +21,7 @@ export default function Header() {
       <div className="md:hidden">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          class="h-10 w-10"
+          className="h-10 w-10"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
